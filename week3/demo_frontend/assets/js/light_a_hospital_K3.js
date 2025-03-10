cdc47b0ce9a08b3056d6d0b5dd2f2c7cb1b6686b
None0003 <khanhlq.study@gmail.com>
2025-03-10 17:09:11 +0700

    update

--- week3/demo_frontend/assets/js/light_a_hospital_K3.js
+++ week3/demo_frontend/assets/js/light_a_hospital_K3.js
@@ -1,27 +1,23 @@
 $(document).ready(function() {
     $('.selectSigner').select2({
         placeholder: "Chọn người ký",
-        allowClear: true
     });
 });
 
 $(document).ready(function() {
     $('.selectTag').select2({
         placeholder: "Chọn thẻ tag",
-        allowClear: true
     });
 });
 
 $(document).ready(function() {
     $('.selectTaker').select2({
         placeholder: "Chọn người chụp",
-        allowClear: true
     });
 });
 
 $(document).ready(function() {
     $('.selectNurse').select2({
         placeholder: "Chọn y tá",
-        allowClear: true
     });
 });
